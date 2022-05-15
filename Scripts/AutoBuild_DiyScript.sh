e820eb5aceefcad62c8f4e42b1c8cddf06c42aa8
tangwulin <45118263+tangwulin@users.noreply.github.com>
2022-05-15 10:26:14 +0800

    Update AutoBuild_DiyScript.sh

--- Scripts/AutoBuild_DiyScript.sh
+++ Scripts/AutoBuild_DiyScript.sh
@@ -4,13 +4,13 @@
 
 Firmware_Diy_Core() {
 
-	Author=AUTO
+	Author="广东千岩信息集团"
 	Author_URL=AUTO
 	Default_FLAG=AUTO
 	Default_IP="192.168.1.1"
 	Banner_Message="Powered by AutoBuild-Actions"
 
-	Short_Firmware_Date=true
+	Short_Firmware_Date=false
 	Checkout_Virtual_Images=false
 	Firmware_Format=AUTO
 	REGEX_Skip_Checkout="packages|buildinfo|sha256sums|manifest|kernel|rootfs|factory"
@@ -40,5 +40,17 @@ Firmware_Diy() {
 	# ${FEEDS_PKG}			OpenWrt 源码目录下的 package/feeds/packages 目录
 	# ${BASE_FILES}			OpenWrt 源码目录下的 package/base-files/files 目录
 
-	:
+	case "${OP_Maintainer}/${OP_REPO_NAME}:${OP_BRANCH}" in
+	coolsnowwolf/lede:master)
+		sed -i "s?/bin/login?/usr/libexec/login.sh?g" ${feeds_pkgs}/ttyd/files/ttyd.config
+		sed -i "/DEVICE_COMPAT_VERSION := 1.1/d" target/linux/ramips/image/mt7621.mk
+	;;
+	esac
+
+	case "${TARGET_PROFILE}" in
+	d-team_newifi-d2)
+		patch -i ${CustomFiles}/mac80211_d-team_newifi-d2.patch package/kernel/mac80211/files/lib/wifi/mac80211.sh
+		Copy ${CustomFiles}/system_d-team_newifi-d2 ${base_files}/etc/config system
+	;;
+	esac
 }
